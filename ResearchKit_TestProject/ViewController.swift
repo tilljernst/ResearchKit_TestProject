0f45040f3f51590f68e2a047c25b88a69d3e4372
Till J. Ernst <tilljernst@gmail.com>
2017-05-11 17:51:20 +0200

    Initial Commit

---
+++ ResearchKit_TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResearchKit_TestProject
+//
+//  Created by Till J. Ernst on 11.05.17.
+//  Copyright © 2017 Till J. Ernst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
