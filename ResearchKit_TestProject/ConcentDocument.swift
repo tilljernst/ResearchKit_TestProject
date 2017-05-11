3ccdc481a023e87f9057f5e1ad222ffd46941bc8
Till J. Ernst <tilljernst@gmail.com>
2017-05-11 19:37:15 +0200

    First Consence Document

---
+++ ResearchKit_TestProject/ConcentDocument.swift
@@ -0,0 +1,41 @@
+//
+//  ConcentDocument.swift
+//  ResearchKit_TestProject
+//
+//  Created by Till J. Ernst on 11.05.17.
+//  Copyright © 2017 Till J. Ernst. All rights reserved.
+//
+
+import ResearchKit
+import Foundation
+
+public var ConsentDocument: ORKConsentDocument{
+    let consentDocument = ORKConsentDocument()
+    consentDocument.title = "Example Consent"
+    
+    // consent sections
+    let consentSectionTypes: [ORKConsentSectionType] = [
+        .overview,
+        .dataGathering,
+        .privacy,
+        .dataUse,
+        .timeCommitment,
+        .studySurvey,
+        .studyTasks,
+        .withdrawing
+    ]
+    
+    let consentSections: [ORKConsentSection] = consentSectionTypes.map { contentSectionType in
+        let consentSection = ORKConsentSection(type: contentSectionType)
+        consentSection.summary = "If you wish to complete this study..."
+        consentSection.content = "In this study you will be asked five (wait, no, three!) questions. You will also have your voice recorded for ten seconds."
+        return consentSection
+    }
+    
+    consentDocument.sections = consentSections
+    
+    // signature
+    consentDocument.addSignature(ORKConsentSignature(forPersonWithTitle: nil, dateFormatString: nil, identifier: "ConsentDocumentParticipantSignature"))
+    
+    return consentDocument
+}
--- ResearchKit_TestProject/ViewController.swift
+++ ResearchKit_TestProject/ViewController.swift
@@ -6,10 +6,26 @@
 //  Copyright © 2017 Till J. Ernst. All rights reserved.
 //
 
+import ResearchKit
 import UIKit
+
+extension ViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
+    
+}
 
 class ViewController: UIViewController {
 
+    @IBAction func consentTapped(_ sender: Any) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+//    func HelloWorld(){
+//        let step1 = ORKInstructionStep(identifier: "step1")
+//        step1.text = "Hello World"
+//        let step2 = ORKInstructionStep(identifier: "step 2")
+//        step2.text = "Bye"
+//        let task = ORKOrderedTask(identifier: "task", steps: [step1, step2])
+//        let taskViewController = ORKTaskViewController(task: task, taskRun: nil)
+//        taskViewController.delegate = self
+//        //presentedViewController(
+//    }
 
 }
 
