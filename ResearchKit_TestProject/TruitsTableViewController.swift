cea0d7352e76c4126fd238665a2ee5db3f59401a
Till J. Ernst <tilljernst@gmail.com>
2017-05-22 12:32:27 +0200

    Fruit Pictures for TableView

--- ResearchKit_TestProject/TruitsTableViewController.swift
+++ ResearchKit_TestProject/TruitsTableViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class TruitsTableViewController: UITableViewController {
 
+    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
+    
     // Mark: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return fruits.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
         
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        let fruitName = fruits[indexPath.row]
+        cell.textLabel?.text = fruitName
+        cell.detailTextLabel?.text = "Delicious!"
+        cell.imageView?.image = UIImage(named: fruitName)
         
         return cell
     }
